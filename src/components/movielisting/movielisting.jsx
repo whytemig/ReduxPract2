7bca4f81f08c7dece57eb87076b583cf7c5aa46f
Miguel <whytemig@gmail.com>
2023-09-19 21:51:53 -0400

    redux shows

--- src/components/movielisting/movielisting.jsx
+++ src/components/movielisting/movielisting.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movieSlice";
 import MovieCard from '../moviecard/moviecard'
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
 
-  let renderMovies = "";
+  let renderMovies, renderShows = "";
 
   renderMovies =
     movies.Response === "True" ? (
     movies.Search.map((movie, index) => {
         return <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className="movie-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+  renderShows =
+    shows.Response === "True" ? (
+    shows.Search.map((show, index) => {
+        return <MovieCard key={index} data={show} />;
       })
     ) : (
       <div className="movie-error">
@@ -25,6 +36,12 @@ const MovieListing = () => {
         <h2>Movies</h2>
         <div className="movie-container">
           {renderMovies}
+        </div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">
+          {renderShows}
         </div>
       </div>
     </div>
--- src/features/movieSlice.js
+++ src/features/movieSlice.js
@@ -1,20 +1,70 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import URL from "../components/api/movieapi";
+import { apiKey } from "../components/api/movieapiKey";
 
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "spiderman";
+    try {
+      const response = await URL.get(
+        `?apiKey=${apiKey}&s=${movieText}&type=movie`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+);
+
+export const fetchAsyncShows = createAsyncThunk(
+  "movies/fetchAsyncShows",
+  async () => {
+    const seriesText = "seinfeld";
+    try {
+      const response = await URL.get(
+        `?apiKey=${apiKey}&s=${seriesText}&type=series`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+);
 
 const initialState = {
-    movies:{}
-}
+    movies: {},
+    shows: {},
+};
 
 const movieSlice = createSlice({
-    name: "movies",
-    initialState,
-    reducers: {
-        addMovies: (state, action) => {
-            state.movies = action.payload;
-        }
-    }
-})
+  name: "movies",
+  initialState,
+  reducers: {
+    addMovies: (state, action) => {
+      state.movies = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("FullFilled");
+      return { ...state, movies: payload };
+      },
+    [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+      console.log("Shows FullFilled");
+      return { ...state, shows: payload };
+      },
+    
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected");
+    },
+  },
+});
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
 export default movieSlice.reducer;
