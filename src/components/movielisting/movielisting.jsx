80dbfb2800d157899a24675a4396e6c171f249b8
Miguel <whytemig@gmail.com>
2023-09-19 21:19:10 -0400

    render movies

--- src/components/movielisting/movielisting.jsx
+++ src/components/movielisting/movielisting.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { getAllMovies } from '../../features/movieSlice';
+import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/movieSlice";
+import MovieCard from '../moviecard/moviecard'
 
-const movielisting = () => {
-
+const MovieListing = () => {
   const movies = useSelector(getAllMovies);
-  console.log(movies);
-  return (
-    <div>movielisting</div>
-  )
-}
 
-export default movielisting
+  let renderMovies = "";
+
+  renderMovies =
+    movies.Response === "True" ? (
+    movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className="movie-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+     
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">
+          {renderMovies}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieListing;
